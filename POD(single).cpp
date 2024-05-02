62683805ed5be730c8a23da085c88418001fabc0
Thank_you <74777405+Md-Akram-Khan@users.noreply.github.com>
2024-05-03 01:17:44 +0600

    Update POD(single).cpp

--- POD(single).cpp
+++ POD(single).cpp
@@ -3,10 +3,10 @@ using namespace std;
 unsigned long long POD(long long n)
 {
     long long m=n;
-    int ans=1;
-    for(int i=2; i*i<=m; i++)
+    long long ans=1;
+    for(long long i=2; i*i<=m; i++)
     {
-        int c=0;
+        long long c=0;
         while(m%i==0)
         {
             m/=i;
@@ -18,7 +18,7 @@ unsigned long long POD(long long n)
         ans*=2;
     if(ans%2==1)
     {
-        int p=sqrt(n);
+        long long p=sqrt(n);
         return pow(n,ans/2)*p;
     }
     else
